7de9f4c9f44d99c3fb7fb8967707169127cd693d
bpoblette <bpoblette@zagmail.gonzaga.edu>
2026-01-22 15:08:24 -0800

    added the model, and have succesfully rendered it in my home webpage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 const Home = () => {
   return (
@@ -12,7 +13,12 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island />
         </Suspense>
       </Canvas>
     </section >
