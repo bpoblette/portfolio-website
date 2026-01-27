a55ac09bd7075e4624dcb17efe4e0fed4b904a96
bpoblette <bpoblette@zagmail.gonzaga.edu>
2026-01-27 15:36:17 -0800

    finished the home page

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons'
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div className='info-box neo-brutalism-blue'>
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+            <img src={arrow} className='w-4 h-4 object-contain' />
+        </Link>
+    </div>
+)
 
 const renderContent = {
     1: (<h1 className='sm:text=xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-        Hi, I am <span className='font-semibold'>Brandon</span>👋
+        Hi, I am <span className='font-semibold'>Brandon</span> 👋
         <br />
         A Software Engineer from Washington State.
     </h1>
     ),
-    2: (<h1>2</h1>
+    2: (<h1>
+        <InfoBox
+            text="Worked on many projects and picked up many skills along the way"
+            link="/about"
+            btnText="Learn more"
+        />
+    </h1>
 
     ),
-    3: (<h1>3</h1>
+    3: (<h1>
+        <InfoBox
+            text="Led multiple projects to success over the years. Curious about the impact?"
+            link="/projects"
+            btnText="Visit my portfolio"
+        />
+    </h1>
 
     ),
-    4: (<h1>4</h1>
+    4: (<h1>
+        <InfoBox
+            text="Need a project done or looking for a dev? I'm just a few keystroke away"
+            link="/contact"
+            btnText="Let's talk"
+        />
+    </h1>
 
     )
 }
-
-const InfoBox = ({ text, link, btnText }) => (
-    <div className='info-box neo-brutalism-blue'>
-        {text}
-    </div>
-)
 
 const HomeInfo = ({ currentStage }) => {
     return renderContent[currentStage] || null;
