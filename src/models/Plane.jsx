3729df229d8e9b632d892c3d0932c104aad0575d
bpoblette <bpoblette@zagmail.gonzaga.edu>
2026-01-27 12:17:33 -0800

    adding animations to the bird

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 import planeScene from '../assets/3d/plane.glb'
 import { useAnimations, useGLTF } from '@react-three/drei'
