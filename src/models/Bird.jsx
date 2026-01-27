6a44087e4ee5a027142b234f55fa0105bb63307e
bpoblette <bpoblette@zagmail.gonzaga.edu>
2026-01-27 15:21:21 -0800

    fixed the tailwind index.css folder so that all the layers work properly. Also finished the bird animation

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -13,8 +13,23 @@ const Bird = () => {
         actions['Take 001'].play();
     }, []);
 
-    useFrame((_, delta) => {
-        birdRef.current.rotation.x += 0.15 * delta
+    useFrame(({ clock, camera }) => {
+        // Update the y position siumulate the flight moving in a sin wave
+        birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+        if (birdRef.current.position.x > camera.position.x + 10) {
+            birdRef.current.rotation.y = Math.PI;
+        } else if (birdRef.current.position.x < camera.position.x - 10) {
+            birdRef.current.rotation.y = 0;
+        }
+
+        if (birdRef.current.rotation.y === 0) {
+            birdRef.current.position.x += 0.01;
+            birdRef.current.position.z -= 0.01;
+        } else {
+            birdRef.current.position.x -= 0.01;
+            birdRef.current.position.z += 0.01;
+        }
     })
     return (
         <mesh
